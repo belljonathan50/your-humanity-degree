07ed004bc59c3d340f450f2b2ea88f4ae2b0b73e
Joseph <joseph.larralde@gmail.com>
2021-05-18 16:05:51 +0200

    added controller schema to keep track of current tabs

---
+++ src/server/schemas/controller.js
@@ -0,0 +1,20 @@
+export default {
+  currentPage: {
+    type: 'enum',
+    default: 'welcome',
+    list: [
+      'welcome',
+      'survey1',
+      'flyingWords1',
+      'survey2',
+      'flyingWords2',
+      'survey3',
+      'flyingWords3',
+      'survey4',
+      'flyingWords4',
+      'puzzle1',
+      'end',
+      'thankyou',
+    ],
+  },  
+}
